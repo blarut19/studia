88be718b0abd6c741caa51482c54bf8e3fc83fc0
Błażej <blarut13@gmail.com>
2023-03-14 15:14:03 +0100

    added some code

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggle_education()
+{
+    if(document.getElementById("education").style.display == "none")
+        document.getElementById("education").style.display = "block";
+    else
+        document.getElementById("education").style.display = "none";
+}
