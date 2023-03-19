c4f4e34e5c319cb3b4dd9641b1fc673fd6cfd31e
Blazej <blarut13@gmail.com>
2023-03-19 20:24:23 +0100

    finished

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function toggle_education()
     else
         document.getElementById("education").style.display = "none";
 }
+function random_meter()
+{
+    let m = document.getElementById("meter");
+    m.value = Math.ceil(Math.random()*10);
+}
